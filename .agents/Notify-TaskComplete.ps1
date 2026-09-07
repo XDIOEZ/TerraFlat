6fe7bb15fd5787fd90e35375f03df572d836965f
Ice_Tilapia <202332793@qq.com>
2026-09-08 01:14:13 +0800

    chore: 加入任务完成通知并强化提交保护

---
+++ .agents/Notify-TaskComplete.ps1
@@ -0,0 +1,30 @@
+param(
+    [string]$Title = "ChatGPT",
+    [string]$Message = "Task completed.",
+    [int]$DurationMilliseconds = 5000
+)
+
+$ErrorActionPreference = "Stop"
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$notifyIcon = New-Object System.Windows.Forms.NotifyIcon
+
+try {
+    $notifyIcon.Icon = [System.Drawing.SystemIcons]::Information
+    $notifyIcon.BalloonTipTitle = $Title
+    $notifyIcon.BalloonTipText = $Message
+    $notifyIcon.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
+    $notifyIcon.Visible = $true
+    $notifyIcon.ShowBalloonTip($DurationMilliseconds)
+
+    $deadline = (Get-Date).AddMilliseconds([Math]::Max($DurationMilliseconds, 1000))
+    while ((Get-Date) -lt $deadline) {
+        [System.Windows.Forms.Application]::DoEvents()
+        Start-Sleep -Milliseconds 100
+    }
+}
+finally {
+    $notifyIcon.Dispose()
+}
